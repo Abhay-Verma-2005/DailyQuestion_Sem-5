e9f0b808da2ec9ffba0afa4c91e54f370792e40a
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2025-08-05 21:40:31 +0530

    JAVA

---
+++ Questions K/Max_Consecutoive_Ones.java
@@ -0,0 +1,21 @@
+public class Max_Consecutoive_Ones {
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 0, 1, 1, 1, 0, 1, 1, 0};
+        int ans = findMaxConsecutiveOnes(arr);
+        System.out.println(ans);
+    }
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+            } else {
+                count = 0;
+            }
+            max = Math.max(count, max);
+        }
+        return max;
+    }
+}
