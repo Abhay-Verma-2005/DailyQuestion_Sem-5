3f9f0b31f868329778795195359e928643a68287
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2025-08-07 21:33:43 +0530

    Add files via upload

---
+++ express.js
@@ -0,0 +1,44 @@
+// express is a framework for NODE js
+
+
+
+
+// express ka use krke server bana skte h /application
+
+// app is the entire instance of the application
+
+// pack.json is most important 
+
+// package.json
+// |-- basic informations
+// |-- dependencies
+// |-- scripts
+// |-- version
+
+
+// GET request
+// POST request
+// PUT request
+// DELETE request
+// PATCH request
+
+// THE GET METHOD IS USED TO RETRIEVE DATA FROM THE SERVER
+// THE POST METHOD IS USED TO SEND DATA TO THE SERVER
+// THE PUT METHOD IS USED to replace all the current representation TO UPDATE EXISTING DATA ON THE SERVER
+// THE PATCH METHOD IS USED TO APPLY PARTIAL UPDATES TO DATA ON THE SERVER
+// THE DELETE METHOD IS USED TO DELETE DATA FROM THE SERVER
+
+
+
+// whenever u=you make any chages in the code, you have to restart the server
+
+// architecture -> way of writing code
+
+// mvc -> model view controller
+
+// views -> templates   -> UI (pages) 
+//            |-->ejs (embedded js tempalate engine) -> html + js
+//            |-->pug
+//            |-->hps
+
+
